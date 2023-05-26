32b1f9368b2872b241fec0a3c1bc70a9c4b6920d
Julie Koubova <julie@koubova.net>
2023-05-26 19:35:37 +0200

    boink

--- esphome/entryway-inkplate.h
+++ esphome/entryway-inkplate.h
@@ -4,12 +4,38 @@
 class MainPage {
 public:
   explicit MainPage(display::DisplayBuffer &buffer)
-    : buffer_(buffer), width_(buffer.get_width()), h_center_{width_ / 2} {}
+    : buffer_(buffer),
+      width_(buffer.get_width()),
+      height_(buffer.get_height()),
+      h_center_{width_ / 2} {}
 
   void draw() const {
     constexpr int margin = 4;
 
     buffer_.fill(COLOR_ON);
+
+    char buffer[64];
+    size_t ret = id(esptime).now().strftime(buffer, sizeof(buffer), "%H:%M");
+    if (ret > 0) {
+      int x1, y1, w, h;
+      buffer_.get_text_bounds (
+        0,
+        0,
+        buffer,
+        &id(medium),
+        TextAlign::TOP_LEFT,
+        &x1, &y1, &w, &h
+      );
+      buffer_.print(
+        rand() % (width_ - (w + 2 * margin)) + margin,
+        rand() % (height_ - (h + 2 * margin)) + margin,
+        &id(medium),
+        COLOR_OFF,
+        TextAlign::TOP_LEFT,
+        buffer
+      );
+    }
+
     buffer_.strftime(
       margin, 
       width_ - margin, 
@@ -25,6 +51,7 @@ public:
 private:
   display::DisplayBuffer &buffer_;
   int width_;
+  int height_;
   int h_center_;
 
 };
